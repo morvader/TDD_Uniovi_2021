c04b51edcaa02088253cd678155f1260c8b37442
Francisco Moreno <francisco.moreno@sngular.com>
2021-04-28 18:12:51 +0200

    FizzBuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,13 @@
 public class FizzBuzz {
     public String getValor(int numero) {
-        return "Fizz";
+
+        if(numero % 3 == 0 && numero % 5 == 0)
+            return "FizzBuzz";
+        else if(numero % 3 == 0)
+            return "Fizz";
+        else if(numero % 5 == 0)
+            return "Buzz";
+
+        return String.valueOf(numero);
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -5,16 +6,53 @@ import static org.hamcrest.Matchers.is;
 
 public class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     void NumeroDivisible3_Fizz() {
         int numero = 3;
         String resultadoEsperado = "Fizz";
 
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         String resultado = fizzBuzz.getValor(numero);
 
         assertThat("No ha devuelto Fizz", resultado, is(resultadoEsperado));
+
+    }
+
+    @Test
+    void NumeroDivisible5_Bizz() {
+        int numero = 5;
+        String resultadoEsperado = "Buzz";
+
+        String resultado = fizzBuzz.getValor(numero);
+
+        assertThat("No ha devuelto Buzz", resultado, is(resultadoEsperado));
+
+    }
+
+    @Test
+    void NumeroDivisible3y5_FizzBizz() {
+        int numero = 15;
+        String resultadoEsperado = "FizzBuzz";
+
+        String resultado = fizzBuzz.getValor(numero);
+
+        assertThat("No ha devuelto FizzBuzz", resultado, is(resultadoEsperado));
+
+    }
+    @Test
+    void NumeroNormal_Numero() {
+        int numero = 1;
+        String resultadoEsperado = "1";
+
+        String resultado = fizzBuzz.getValor(numero);
+
+        assertThat("No ha devuelto el numero", resultado, is(resultadoEsperado));
 
     }
 }
